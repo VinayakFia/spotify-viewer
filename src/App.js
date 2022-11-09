2de4c14f226869b646bf8fe5c9f87d9f28c2ce12
VinayakFia <vinayak.fialok@outlook.com>
2022-11-09 20:31:53 +1100

    boilerplate

---
+++ src/App.js
@@ -0,0 +1,86 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { RESPONSE_TYPE, CLIENT_ID, AUTH_ENDPOINT, REDIRECT_URI } from "./config";
+
+function App() {
+  const [token, setToken] = useState("");
+  const [searchKey, setSearchKey] = useState("");
+  const [artists, setArtists] = useState([]);
+
+  useEffect(() => {
+    const hash = window.location.hash;
+    let token = window.localStorage.getItem("token");
+
+    if (!token && hash) {
+      token = hash
+        .substring(1)
+        .split("&")
+        .find((elem) => elem.startsWith("access_token"))
+        .split("=")[1];
+
+      window.location.hash = "";
+      window.localStorage.setItem("token", token);
+    }
+
+    setToken(token);
+  }, []);
+
+  const logout = () => {
+    setToken("");
+    window.localStorage.removeItem("token");
+  };
+
+  const searchArtists = async (e) => {
+    e.preventDefault();
+    const { data } = await axios.get("https://api.spotify.com/v1/search", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      params: {
+        q: searchKey,
+        type: "artist",
+      },
+    });
+
+    setArtists(data.artists.items);
+  };
+
+  const renderArtists = () => {
+    return artists.map((artist) => (
+      <div key={artist.id}>
+        {artist.images.length ? (
+          <img width={"100%"} src={artist.images[0].url} alt="" />
+        ) : (
+          <div>No Image</div>
+        )}
+        {artist.name}
+      </div>
+    ));
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Spotify React</h1>
+        {!token ? (
+          <a
+            href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
+          >
+            Login to Spotify
+          </a>
+        ) : (
+          <button onClick={logout}>Logout</button>
+        )}
+        <form onSubmit={searchArtists}>
+          <input type="text" onChange={(e) => setSearchKey(e.target.value)} />
+          <button type={"submit"}>Search</button>
+        </form>
+        {renderArtists()}
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/config.js
@@ -0,0 +1,5 @@
+export const CLIENT_ID = "0aa32b640bdb4d6e845113b4b2947be2"; // Your client id
+export const CLIENT_SECRET = "416e1098a28a44c1b50aa87a994fdd33"; // Your secret
+export const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+export const REDIRECT_URI = "http://localhost:3000"; // Your redirect uri
+export const RESPONSE_TYPE = "token";
