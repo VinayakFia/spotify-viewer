6bbb84c63a0d3a5ce56a7dcda3a005deeb0a7f03
VinayakFia <vinayak.fialok@outlook.com>
2022-11-10 15:02:03 +1100

    Made login less ugly

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,18 @@
 import React from "react";
-import { RESPONSE_TYPE, CLIENT_ID, AUTH_ENDPOINT, REDIRECT_URI, SCOPE } from "../config";
+import {
+  RESPONSE_TYPE,
+  CLIENT_ID,
+  AUTH_ENDPOINT,
+  REDIRECT_URI,
+  SCOPE,
+} from "../config";
 
 const Login = () => {
   return (
-    <div>
+    <div className="bg-[#131312] w-full h-full absolute text-4xl text-[#FEFFFE] font-bold grid grid-rows-2">
+      <p className="m-auto text-7xl">Spotify Stats</p>
       <a
+        className="rounded-full bg-[#1DB954] p-10 m-auto"
         href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`}
       >
         Login to Spotify
