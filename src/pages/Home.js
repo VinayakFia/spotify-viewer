002e22ff0b366d65bede781ecaa4a2f11bfba092
Vinayak Fialok <vinayak.fialok@outlook.com>
2022-11-13 16:50:33 +1100

    aoisdiofasd

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -125,16 +125,17 @@ const Home = () => {
     return genres.map((x) => (
       <div
         className="col-span-3 relative
-			rounded-full text-[#FEFFFE] font-semibold hover:bg-neutral-800"
+			rounded-full text-[#FEFFFE] font-semibold bg-neutral-800"
       >
+        <div
+          className={`bg-[#1DB954] ${getWidth(
+            x[1] / max
+          )} absolute rounded-full p-3`}
+        >
+          {x[0]}
+        </div>
         <div className="bg-[#222322] rounded-full p-3 text-[#FEFFFE] font-semibold">
-          <div
-            className={`bg-[#1DB954] ${getWidth(
-              x[1] / max
-            )} absolute rounded-full p-3`}
-          >
-            {x[0]}
-          </div>
+          {x[0]}
         </div>
       </div>
     ));
