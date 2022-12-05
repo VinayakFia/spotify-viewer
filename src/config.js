7b06f14a1f1ef1dd985fa62e46bda7c18295c66d
VinayakFia <vinayak.fialok@outlook.com>
2022-12-05 11:43:08 +1100

    fixed rediret

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const CLIENT_ID = "0aa32b640bdb4d6e845113b4b2947be2"; // Your client id
 export const CLIENT_SECRET = "416e1098a28a44c1b50aa87a994fdd33"; // Your secret
 export const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
-export const REDIRECT_URI = "http://localhost:3000"//"https://spotify-viewer-two.vercel.app/"; // Your redirect uri
+export const REDIRECT_URI = "https://spotify-viewer-two.vercel.app/"; // Your redirect uri
 export const RESPONSE_TYPE = "token";
 export const SCOPE = "user-read-private user-read-email user-top-read user-read-recently-played"
